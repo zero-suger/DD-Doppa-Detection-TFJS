232bb6a4260f9bb8d6699eacf987182f6a86a17e
SeongJu Lee <seong201@korea-id.co.kr>
2024-08-08 13:40:00 +0900

    v3

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 2000, // handle warning on vendor.js bundle size
-  },
-  base: "/DD/",
-});
+   publicDir: 'public'
+})
